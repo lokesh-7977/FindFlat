c52429eac9130bd1c5e2a6cc925e61066c6f46e9
Lokesh Medharametla <lokeshchowdary902@gmail.com>
2026-03-25 00:29:06 +0530

    chore: remove old root src and migrations

--- src/db/schema.ts
+++
@@ -1,6 +0,0 @@
-export * from "./auth";
-export * from "./equipment";
-export * from "./events";
-export * from "./flatmates";
-export * from "./flats";
-export * from "./services";
--- src/lib/db.ts
+++
@@ -1,7 +0,0 @@
-export function escapeLike(str: string): string {
-  return str.replace(/%/g, "\\%").replace(/_/g, "\\_");
-}
-
-export function likePattern(str: string): string {
-  return `%${escapeLike(str)}%`;
-}
